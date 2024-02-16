1f7e21459065149e2c42c16f45b3b7fdbde2a203
Makbel116 <makbel9395@gmail.com>
2024-02-16 14:16:55 -0800

    minimized the css and js

---
+++ js/index-mini.js
@@ -0,0 +1,1 @@
+window.onscroll=function(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){$(".navbar").addClass("navbar-active")}else{$(".navbar").removeClass("navbar-active")}}
